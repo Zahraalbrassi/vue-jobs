aff8416b95c50648f5c105def8baff4349dc667b
zahraalbrassi <Zahraalbrassi@gmail.com>
2024-09-21 20:05:25 +0200

    added add a new compoments

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,9 +1,10 @@
 <script setup>
 
-import { ref, defineProps, onMounted } from 'vue';
+import { reactive, defineProps, onMounted } from 'vue';
 import JobListing from './JobListing.vue';
 import { RouterLink } from 'vue-router';
 import axios from 'axios';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 defineProps({
     limit:Number,
     showButton:{
@@ -11,13 +12,18 @@ type:Boolean,
 default: false
     }
 })
-const jobs = ref([]);
+const state = reactive({
+    jobs:[],
+    isLoading:true
+});
 onMounted(async ()=>{
     try{
         const response = await axios.get('http://localhost:5000/jobs');
-        jobs.value = response.data;
+        state.jobs = response.data;
     }catch(error){
  console.error('error fetching data', error)
+    }finally{
+        state.isLoading= false;
     }
     
 });
@@ -26,8 +32,13 @@ onMounted(async ()=>{
     <section class="bg-blue-50 px-4 py-10">
 <div class="container-xl lg:container m-auto" >
  <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
- <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-    <JobListing v-for="job in jobs.slice(0, limit || jobs.length)"
+ <!-- SHOW LOADING SPINNER WHILE LOADING IS TRUE -->
+  <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+    <PulseLoader />
+  </div>
+  <!-- SHOW JOB LISTING WHEN DONE LOADING  -->
+ <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+    <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
      :key="job.id"
       :job="job" 
       />
